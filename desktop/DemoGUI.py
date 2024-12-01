104aea2bcefa82b929aba1f49588458010e8aa4f
Hunter Rabun <hunterrabun@gmail.com>
2024-12-01 11:20:49 -0500

    forgot gui code for webapp

--- desktop/DemoGUI.py
+++ desktop/DemoGUI.py
@@ -1,13 +1,15 @@
 import sys
 from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QComboBox, QDial, QLabel, QPushButton
-from PySide6.QtCore import Qt, QThread, Signal
+from PySide6.QtCore import Qt, QThread, Signal, Slot
 from PySide6.QtGui import QFont
 import serial
 import serial.tools.list_ports
 import time
 import re
 from WindowsVolumeController import WindowsAudioController
-
+from WebAppThread import WebApp
+import json
+    
 number_t = int | float
 def map_value(x: number_t, in_min: number_t, in_max: number_t, out_min: number_t, out_max: number_t) -> float:
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
@@ -93,12 +95,19 @@ class SimpleGui(QWidget):
 
         # Serial Thread
         self.serial_thread = None
+        self.web_app_thread = WebApp()
+        self.web_app_thread.recv_data.connect(self.handle_json)
+        self.web_app_thread.set_start_callback(lambda: {"process": self.dropdown1.currentText(), "volume": self.dial.value()})
+        self.web_app_thread.start()
     def on_process_change(self, index):
         selected_value = self.dropdown1.currentText()
         try:
             self.volume_controller.set_selected_process_name(selected_value)
         except ValueError:
             self.dropdown1.removeItem(index)
+
+    def handle_json(self, data: dict):
+        self.dial.setValue(int(data["volume"]))
 
     def on_endpoint_change(self, index):
         selected_value = self.dropdown2.currentText()
@@ -142,6 +151,8 @@ class SimpleGui(QWidget):
     def update_label(self, value):
         """Update the label with the dial's current value."""
         self.dial_label.setText(f"Volume: {value}%")
+        d = {"process": self.dropdown1.currentText(), "volume": value}
+        self.web_app_thread.trans_data.emit(d)
         self.volume_controller.set_volume(value / 100)
 
 # Run the application
